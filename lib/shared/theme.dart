db65de7b20e0f48aa2b7059f09764fb95595ae20
Anasmc97 <anas.segafmulachela@gmail.com>
2022-03-11 16:32:49 +0800

    added new color theme

---
+++ lib/shared/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+double defaultMargin = 24;
+
+Color mainColor = const Color(0xFF416CE1);
+Color accentColor1 = const Color(0xFFA0B3E2);
+Color navBarColor1 = const Color(0xFF6E8DDD);
+Color navBarColor2 = const Color(0xFF5C7BCD);
+Color searchBarColor = const Color(0xFFEFF2FF);
