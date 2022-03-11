5a88e5cd850ebcc91364a777298afa2a9312c205
rfaturriza <rfaturriza@gmail.com>
2022-03-11 16:20:57 +0700

    update theme textstyle

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -6,6 +6,35 @@ class CustomTheme {
     return ThemeData(
       scaffoldBackgroundColor: CustomColors.white,
       fontFamily: 'Roboto',
+      textTheme: const TextTheme(
+        headline1: TextStyle(
+          fontSize: 30.0,
+          fontWeight: FontWeight.w700,
+          color: CustomColors.black,
+        ),
+        headline2: TextStyle(
+          fontSize: 28.0,
+          fontWeight: FontWeight.w700,
+          color: CustomColors.white,
+        ),
+        headline3: TextStyle(
+          fontSize: 22.0,
+          fontWeight: FontWeight.w500,
+          color: CustomColors.white,
+        ),
+        bodyText1: TextStyle(
+          fontSize: 18.0,
+          color: CustomColors.fontPrimary,
+        ),
+        bodyText2: TextStyle(
+          fontSize: 16.0,
+          color: CustomColors.black,
+        ),
+        caption: TextStyle(
+          fontSize: 14.0,
+          color: CustomColors.black,
+        ),
+      ),
     );
   }
 }
